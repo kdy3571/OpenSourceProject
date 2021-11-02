852c0522fcfa5b9c7849c246fd16cbacab051225
gngnrkfl <84559902+gngnrkfl@users.noreply.github.com>
2021-11-03 00:40:31 +0900

    Update dataGet.c

--- dataGet.c
+++ dataGet.c
@@ -109,7 +109,7 @@ void getINFO(MYSQL *conn, int argc, char *argv[],char *content)
     }
   }
   if(index == 0){
-    sprintf(content, "** Cannot find sensor name: %s\n", name);
+    sprintf(content, "** Cannot find sensor name;%s\n", name);
   }
   else {
     sprintf(content,"%d %.1f %.1f \n", cnt, ave, max);
@@ -146,7 +146,7 @@ void getGET(MYSQL *conn, int argc, char *argv[], char *content)
   }
   
   if(sec == 0){
-    sprintf(content, "** Cannot find sensor name:%s\n", name);
+    sprintf(content, "** Cannot find sensor name;%s\n", name);
   } 
   else {
     sprintf(s_num, "%d", s_number);
