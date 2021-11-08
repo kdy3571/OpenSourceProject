f0a15107c99718c6fcea5c3cbdbaa09a87206ece
gngnrkfl <84559902+gngnrkfl@users.noreply.github.com>
2021-11-08 22:12:50 +0900

    Update alarm.c

--- alarm.c
+++ alarm.c
@@ -1,6 +1,7 @@
 #include "stems.h"
 
 void parseData(char *buf, char *name, char *time, char *value){
+    /*받아온 값을 이름, 시간, 값으로 구분하는 함수*/
     strtok(buf, "="); 
     sprintf(name, "%s", strtok(NULL, "&")); 
 
@@ -17,14 +18,12 @@ int main(void){
     
     char len[MAXBUF];
     int lens;
-  
     sprintf(len, "%s", Getenv("CONTENT_LENGTH"));
     if(!len)
       return (-1);
     lens = atoi(len);
-  
-    Read(STDIN_FILENO, buf, lens);
-    parseData(buf, name, time, value);
+    Read(STDIN_FILENO, buf, lens); //buf 값을 불러온다
+    parseData(buf, name, time, value);  //값을 분리
 
     fprintf(stderr, "경고 : %s sensor로부터 %s 시각에 %s 라는 값이 발생했습니다.\n", name, time, value);
     printf("200 OK\n");
